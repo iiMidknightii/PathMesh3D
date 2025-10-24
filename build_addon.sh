1d97f2da08f88fc44da227a9b77919d6b5c2c144
iiMidknightii <89699177+iiMidknightii@users.noreply.github.com>
2025-10-24 08:44:30 -0700

    Updated to Godot 4.5

--- build_addon.sh
+++ build_addon.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/bash
-exec > "tmp.log" 2>&1
 
 rm -f addons.zip
 
 scons
-../Godot.exe --doctool ./ --gdextension-docs
+godot --doctool ./ --gdextension-docs
 scons target=template_debug
 scons target=template_release
-wsl -e scons target=template_debug
-wsl -e scons target=template_release
+scons platform=windows target=template_debug
+scons platform=windows target=template_release
 
 rm -f addons/*/bin/~*.TMP
 cp LICENSE README.md addons/PathMesh3D
 
 prev_dir=$(basename $PWD)
 cd ..
-wsl zip -9 -r "$prev_dir/addons.zip" "$prev_dir/addons/"
+zip -9 -r "$prev_dir/addons.zip" "$prev_dir/addons/"
 cd "$prev_dir"
-rm -f addons/README.md addons/LICENSE
+rm -f addons/PathMesh3D/README.md addons/PathMesh3D/LICENSE
